b9910eb135884dfe429b7b20808e3465bdec7569
Xinyue Lu <xinyue@xinyue-mac.local>
2022-07-19 10:54:14 -0400

    Still imperfect aftering filtering out cells with no neighbors.

--- bin/downstream.py
+++ bin/downstream.py
@@ -12,19 +12,13 @@ from plot_utils import new_plot
 import numpy as np
 
 def main(
-	anndata_dir: Path
+	muon_ori: Path
 ):
 	# Read the muon data here
-	rna_path = str(anndata_dir) + "/rna_filtered.h5ad"
-	adt_path = str(anndata_dir) + "/adt_filtered.h5ad"
-	hto_path = str(anndata_dir) + "/hto_filtered.h5ad"
-    
-	rna_expr = sc.read_h5ad(rna_path)
-	# rna_expr.obs["log10umi"] = np.array(np.log10(rna_expr.X.sum(axis=1) + 1)).reshape(-1)
-	adt_expr = sc.read_h5ad(adt_path)
-	if exists(hto_path):
-		hto_expr = sc.read_h5ad(hto_path)
-		print("Find HTO data.")
+	rna_expr = mu.read(str(muon_ori / "rna"))
+	rna_expr.X = rna_expr.layers['spliced']
+	adt_expr = mu.read(str(muon_ori / "adt"))
+	
 	print("Construct muon object with RNA and ADT expression data.")
 	mdata_raw = mu.MuData({'rna': rna_expr, 'adt':adt_expr})
 	print(mdata_raw)
@@ -48,6 +42,13 @@ def main(
 	print("Performing downstream analysis for ADT...")
 	sc.tl.pca(mdata_adt)
 	sc.pp.neighbors(mdata_adt)
+	for axis in [0, 1]:
+		neighbor_counts = np.array((mdata_adt.obsp['distances'] > 0).sum(axis=axis)).flatten()
+		mdata_adt.obs.loc[:, f'neighbor_counts_ax{axis}'] = neighbor_counts
+		cells_with_neighbors = (neighbor_counts > 0).astype(int)
+		mdata_adt.obs.loc[:, f'has_neighbors_ax{axis}'] = cells_with_neighbors
+	mdata_adt.obs.loc[:, 'has_neighbors'] = mdata_adt.obs.loc[:, 'has_neighbors_ax0'] * mdata_adt.obs.loc[:, 'has_neighbors_ax1']
+	mu.pp.filter_obs(mdata_adt, 'has_neighbors_ax0', lambda x: x > 0)
 	sc.tl.leiden(mdata_adt)
 	sc.tl.umap(mdata_adt)
 	with new_plot():
@@ -56,7 +57,7 @@ def main(
 	
 	## Downstream analysis for RNA
 	print("Performing downstream analysis for RNA...")
-    
+    	
 	# find highly variable genes
 	sc.pp.highly_variable_genes(mdata_rna, min_mean=0.02, max_mean=4, min_disp=0.5)
 	print("Found", np.sum(mdata_rna.var.highly_variable), "highly variable genes.")
@@ -72,7 +73,14 @@ def main(
 	sc.tl.pca(mdata_rna)
 	print("Constructing neighborhood graph...")
 	sc.pp.neighbors(mdata_rna, n_neighbors=10, n_pcs=20)
-
+	
+	for axis in [0, 1]:
+		neighbor_counts = np.array((mdata_rna.obsp['distances'] > 0).sum(axis=axis)).flatten()
+		mdata_rna.obs.loc[:, f'neighbor_counts_ax{axis}'] = neighbor_counts
+		cells_with_neighbors = (neighbor_counts > 0).astype(int)
+		mdata_rna.obs.loc[:, f'has_neighbors_ax{axis}'] = cells_with_neighbors
+	mdata_rna.obs.loc[:, 'has_neighbors'] = mdata_rna.obs.loc[:, 'has_neighbors_ax0'] * mdata_rna.obs.loc[:, 'has_neighbors_ax1']
+	mu.pp.filter_obs(mdata_rna, 'has_neighbors_ax0', lambda x: x > 0)
 	# clustering
 	print("Performing leiden clustering with the computed neighbourhood graph...")
 	sc.tl.leiden(mdata_rna, resolution=.75)
@@ -80,37 +88,40 @@ def main(
 	with new_plot():
 		sc.pl.umap(mdata_rna, color="leiden", legend_loc="on data")
 		plt.savefig("leiden_cluster_rna.pdf")
-
+	# print(mdata_adt)
+	# print(mdata_rna)
 	## Multi-omics integration
+	print("Performing multi-omics integration...")
 	mdata_raw.update()
     # if we filter the cells at the RNA QC step, subset them in the protein modality
-    # mu.pp.intersect_obs(mdata_raw)
-
+	print("Intersecting the common cells...")
+	mu.pp.intersect_obs(mdata_raw)
+	print(mdata_raw)
 	# multiplex clustering
-	#mu.pp.neighbors(mdata_raw)
-	#mu.tl.umap(mdata_raw)
+	mu.pp.neighbors(mdata_raw)
+	mu.tl.umap(mdata_raw)
 	mu.tl.louvain(mdata_raw, resolution=[2, .1], random_state=1)
 	mu.tl.leiden(mdata_raw, resolution=[2, .1], random_state=1)
 	print(mdata_raw)
-	#with new_plot():
-		#mu.pl.embedding(mdata_raw, basis="X_umap", color="louvain")
-		#plt.savefig("louvain_cluster_combined.pdf")
-	#with new_plot():
-		#mu.pl.embedding(mdata_raw, basis="X_umap", color="leiden")
-		#plt.savefig("leiden_cluster_combined.pdf")
+	with new_plot():
+		mu.pl.embedding(mdata_raw, basis="X_umap", color="louvain")
+		plt.savefig("louvain_cluster_combined.pdf")
+	with new_plot():
+		mu.pl.embedding(mdata_raw, basis="X_umap", color="leiden")
+		plt.savefig("leiden_cluster_combined.pdf")
 
 	
 	# multi-omics factor analysis
 	# generate an interpretable latent space for both RNA and ADT modalities
-	mdata_adt.var["highly_variable"] = True
-	mdata_raw.update()
-	mu.tl.mofa(mdata_raw, outfile="mofa.hdf5", n_factors=30)
-	print(mdata_raw.obsm["X_mofa"].shape)
-	sc.pp.neighbors(mdata_raw, use_rep="X_mofa")
-	sc.tl.umap(mdata_raw, random_state=1)
-	with new_plot():
-        	mu.pl.embedding(mdata_raw, basis="X_umap", color="louvain")
-        	plt.savefig("louvain_cluster_combined.pdf")
+	# mdata_adt.var["highly_variable"] = True
+	# mdata_raw.update()
+	# mu.tl.mofa(mdata_raw, outfile="mofa.hdf5", n_factors=30)
+	# print(mdata_raw.obsm["X_mofa"].shape)
+	# sc.pp.neighbors(mdata_raw, use_rep="X_mofa")
+	# sc.tl.umap(mdata_raw, random_state=1)
+	# with new_plot():
+        	# mu.pl.embedding(mdata_raw, basis="X_umap", color="louvain")
+        	# plt.savefig("louvain_cluster_combined.pdf")
 	#with new_plot():
         	#mu.pl.embedding(mdata_raw, basis="X_umap", color="leiden")
 	        #plt.savefig("leiden_cluster_combined.pdf")
@@ -119,10 +130,10 @@ def main(
 	
 if __name__ == "__main__":
 	p = ArgumentParser()
-	p.add_argument("--anndata_dir", type=Path)
+	p.add_argument("--muon_ori", type=Path)
 
 	args = p.parse_args()
 
 	main(
-		args.anndata_dir,
+		args.muon_ori,
 	)
